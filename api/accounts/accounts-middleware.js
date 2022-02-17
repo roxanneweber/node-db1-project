4aad25db5dc235c078d3e23928d5464e24839349
Roxanne Weber <marie.sankofa@gmail.com>
2022-02-17 10:17:39 -0600

    completed middleware for getById router

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,8 +1,9 @@
+const Accounts = require('./accounts-model')
+
 exports.checkAccountPayload = (req, res, next) => {
 	console.log('checkAccountPayload middleware');
 	next();
-	// Note: you can either write "manual" validation logic
-	// or use the Yup library (not currently installed)
+
 };
 
 exports.checkAccountNameUnique = (req, res, next) => {
@@ -10,7 +11,17 @@ exports.checkAccountNameUnique = (req, res, next) => {
 	next();
 };
 
-exports.checkAccountId = (req, res, next) => {
-	console.log('checkAccountId middleware');
-	next();
+exports.checkAccountId = async (req, res, next) => {
+	try {
+		const account = await Accounts.getById(req.params.id)
+		if (!account) {
+			next({ status: 404, message: 'the account was not found'})
+		} else {
+			req.account = account
+			next()
+		}
+	} catch {
+		next(err);
+	}
+	
 };
